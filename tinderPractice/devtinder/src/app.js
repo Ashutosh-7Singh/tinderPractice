df8ece85b6717cf03b0db288070d18dad1ce1987
Ashutosh-7Singh <ashutosh.singh@thegoacademy.com>
2024-10-27 01:18:07 +0530

    updated

--- tinderPractice/devtinder/src/app.js
+++ tinderPractice/devtinder/src/app.js
@@ -1,22 +1,28 @@
-const express= require("express");
+const express = require("express");
+const app = express();
 
-const app=express();
-
-app.use("/user",(req,res)=>{
-    res.send("test from server 3002")
-})
-
-app.get("/user",(req,res)=>{
-    res.send({firstName:"Ashutosh Singh",lastName:"Moni Singh"});
-})
-app.post("/user",(req,res)=>{
-    res.send("User data saved sucessfully")
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("User deleted sucessfully ")
-})
-app.listen(3002,()=>{
-    console.log("Server listening on port 3002");
+app.use("/user", 
+    (req, res,next) => {
+  console.log("Hmadeling the route handler");
+//   res.send("first route handler")
+next()
+},
+(req,res)=>{
+    console.log("handeling the second route handler");
+    res.send("2nd route handler")
     
-})
+},
+(req,res)=>{
+  console.log("handeling the second route handler");
+  res.send("2nd route handler")
+  
+},
+(req,res)=>{
+  console.log("handeling the second route handler");
+  res.send("2nd route handler")
+  
+});
+
+app.listen(3002, () => {
+  console.log("Server successfully listening on the 3002......");
+});

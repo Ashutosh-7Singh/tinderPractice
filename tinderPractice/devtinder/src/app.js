abca4af6c5bcb3e4253d465c2a653646ea1ca1e6
Ashutosh-7Singh <ashutosh.singh@thegoacademy.com>
2024-10-27 02:15:11 +0530

    next update

--- tinderPractice/devtinder/src/app.js
+++ tinderPractice/devtinder/src/app.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.use("/user", 
-    (req, res,next) => {
-  console.log("Hmadeling the route handler");
-//   res.send("first route handler")
-next()
-},
-(req,res)=>{
-    console.log("handeling the second route handler");
-    res.send("2nd route handler")
-    
-},
-(req,res)=>{
-  console.log("handeling the second route handler");
-  res.send("2nd route handler")
-  
-},
-(req,res)=>{
-  console.log("handeling the second route handler");
-  res.send("2nd route handler")
-  
-});
+app.get(
+  "/user",(req,res,next)=>{
+    console.log("Handling the route user 1!!");
+    res.send("first route handler");
+    next()
+  }
+)
 
+app.get(
+  "/user",(req,res)=>{
+    console.log("Handling the sencond route");
+    res.send("second route handler");
+    
+  }
+)
 app.listen(3002, () => {
   console.log("Server successfully listening on the 3002......");
 });

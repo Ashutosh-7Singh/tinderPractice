4e34d7c09f94b643b60c01c0ad6684efec065af8
Ashutosh-7Singh <ashutosh.singh@thegoacademy.com>
2024-10-27 14:28:43 +0530

    token

--- tinderPractice/devtinder/src/app.js
+++ tinderPractice/devtinder/src/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get(
+  "/user",(req,res)=>{
+    console.log("Handling the sencond route");
+    res.send("second route handler");
+    
+  }
+)
+app.get(
   "/user",(req,res,next)=>{
     console.log("Handling the route user 1!!");
     res.send("first route handler");
@@ -9,13 +16,7 @@ app.get(
   }
 )
 
-app.get(
-  "/user",(req,res)=>{
-    console.log("Handling the sencond route");
-    res.send("second route handler");
-    
-  }
-)
+
 app.listen(3002, () => {
   console.log("Server successfully listening on the 3002......");
 });
